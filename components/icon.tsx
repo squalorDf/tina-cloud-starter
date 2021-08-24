617629feec97e3496e64107b734b49c96eaa30c6
squalorDf <egyabig2@gmail.com>
2021-08-24 16:21:46 +0300

    Update icon.tsx

--- components/icon.tsx
+++ components/icon.tsx
@@ -41,7 +41,7 @@ import { FaBeer, FaCoffee, FaPalette } from "react-icons/fa";
 // @ts-ignore
 import TinaIconSvg from "../public/tina.svg";
 // добавляем лого с рейвом
-import RvveIconSvg from "../public/rvve_logo.svg";
+// import RvveIconSvg from "../public/rvve_logo.svg";
 
 const biIconOptions = {
   code: BiCodeBlock,
@@ -64,7 +64,7 @@ const biIconOptions = {
   world: BiWorld,
   aperture: FiAperture,
   tina: TinaIconSvg,
-  rvve: RvveIconSvg,
+  // rvve: RvveIconSvg,
 };
 
 const heroIconOptions = {
@@ -88,7 +88,7 @@ const heroIconOptions = {
   world: BiWorld,
   aperture: FiAperture,
   tina: TinaIconSvg,
-  rvve: RvveIconSvg,
+  // rvve: RvveIconSvg,
 };
 
 export const Icon = ({ data, parentColor = "", className = "" }) => {
